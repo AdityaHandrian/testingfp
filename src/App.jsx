d80eaacb7fb2b7276cfde43ee94e4376346deea6
Durian <adityahandriann@gmail.com>
2025-12-06 14:10:38 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="text-center p-10 bg-white shadow-xl rounded-lg">
+        <h1 className="text-4xl font-bold text-blue-600 mb-4">
+          RSBP Frontend Ready!
+        </h1>
+        <p className="text-gray-600">
+          Environment setup, Tailwind, & Vite berhasil diinstal.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
